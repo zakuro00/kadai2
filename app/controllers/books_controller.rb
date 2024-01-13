7c090fe84eb03c4d7bdf2fea3a24886435070050
EC2 Default User <ec2-user@ip-172-31-39-183.ap-northeast-1.compute.internal>
2024-01-13 07:54:37 +0000

    start

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def show
@@ -12,7 +13,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_show_path(@book.id), notice: "Book created successfully."
+      redirect_to book_path(@book.id), notice: "Book created successfully."
     else
       flash[:alert] = "errors prohibited this book from being saved:"
       @books = Book.all
@@ -26,17 +27,20 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_show_path(@book.id)
-    flash[:notice] = "Book was successfully updated."
-    
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      flash.now[:alert]="error"
+      render :edit 
+    end
   end
   
   def destroy
    @book = Book.find(params[:id])
    @book.destroy
    flash[:success] = "Book was successfully destroyed."
-   redirect_to books_path(@book.id)
+   redirect_to books_path
   end
  
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'books/:id' => 'books#show' ,as: 'book_show'
-  get 'books/new'
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id/edit' => 'books#edit' , as: 'edit_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  #root to: 'homes#top'
+  #get 'books/:id' => 'books#show' ,as: 'book'
+  #get 'books/new'
+  #get 'books' => 'books#index'  ,as: 'books'
+  #post 'books' => 'books#create'
+  #get 'books/:id/edit' => 'books#edit' , as: 'edit_book'
+  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  #patch 'books/:id' => 'books#update', as: 'update_book'
+  resources :books
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
